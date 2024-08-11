8d79530f787db267f5f2ed18b73b8ab20c315334
Basit <basit2054754@gmail.com>
2024-08-11 10:40:42 +0500

    Update ping.js

--- src/commands/Community/ping.js
+++ src/commands/Community/ping.js
@@ -9,36 +9,30 @@ module.exports = {
 
     async execute(interaction, client) {
         const startTime = Date.now();
+
         await interaction.deferReply({ ephemeral: true });
 
         const deferEndTime = Date.now();
         const deferTime = deferEndTime - startTime;
 
-        await interaction.editReply({ content: "<a:Loading:1226093096933265480>" });
+        await interaction.editReply({ content: "<a:Refreshing:1272062448915976322>" });
 
-        const editEndTime = Date.now();
-        const roundTripLatency = editEndTime - startTime;
-
+        const roundTripLatency = Date.now() - startTime;
         const apiLatency = Math.round(client.ws.ping);
 
-        let statusEmoji;
-        if (roundTripLatency < 100) {
-            statusEmoji = '🟢';
-        } else if (roundTripLatency < 400) {
-            statusEmoji = '🟡';
-        } else {
-            statusEmoji = '🔴';
-        }
+        const statusEmoji = roundTripLatency < 800 ? '<a:Wifi_Green:1271842106297810996>' : roundTripLatency < 1600 ? '<a:Wifi_Yellow:1271845367142551653>' : '<a:Wifi_Red:1271840634650497054>';
 
         const embed = new EmbedBuilder()
             .setColor('#2B2D31')
-            .setTitle('Pong!')
-            .setDescription('Latency Details:')
+            .setTitle('Pong! Latency Information')
+            .setDescription('Here are the details:')
             .addFields(
                 { name: 'Round-trip Latency', value: `${roundTripLatency}ms ${statusEmoji}`, inline: true },
                 { name: 'API Latency', value: `${apiLatency}ms`, inline: true },
-                { name: 'Defer Time', value: `${deferTime}ms`, inline: true }
-            );
+                { name: 'Response Delay', value: `${deferTime}ms`, inline: true }
+            )
+            .setFooter({ text: `Requested by ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() })
+            .setTimestamp();
 
         await interaction.editReply({ content: null, embeds: [embed] });
     },
