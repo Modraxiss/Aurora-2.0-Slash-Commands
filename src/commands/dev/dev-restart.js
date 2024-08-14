8d01fbb8e494fb34b72a4b5dc509edaadfcc71ba
Basit <basit2054754@gmail.com>
2024-08-14 18:30:37 +0500

    Update dev-restart.js

--- src/commands/dev/dev-restart.js
+++ src/commands/dev/dev-restart.js
@@ -13,68 +13,66 @@ module.exports = {
 
         const ownerId = process.env.botOwnerID;
 
-        if (!ownerId) return interaction.editReply({ content: "The bot owner ID is not specified, unable to verify authorization." });
+        if (!ownerId) return interaction.editReply({ content: "<a:Warning:1264409367256502453> The bot owner ID is not specified, unable to verify authorization." });
 
-        if (interaction.user.id !== ownerId) return interaction.editReply({ content: "You don't have permission to execute this command." });
+        if (interaction.user.id !== ownerId) return await interaction.editReply({ content: "<:Note:1271777744740417571> You don't have permission to execute this command.\nOnly <:DEV:1271794354398167142> developers can use this command." });
 
-        const row = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('restart_yes')
-                    .setLabel('Yes')
-                    .setStyle(ButtonStyle.Success),
-                new ButtonBuilder()
-                    .setCustomId('restart_no')
-                    .setLabel('No')
-                    .setStyle(ButtonStyle.Danger),
-            );
+        const buttons = [
+            new ButtonBuilder()
+                .setCustomId('restart_yes')
+                .setLabel('Yes')
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setCustomId('restart_no')
+                .setLabel('No')
+                .setStyle(ButtonStyle.Danger),
+        ];
 
-        await interaction.editReply({ content: 'Are you sure you want to restart the bot?', components: [row], });
+        const row = new ActionRowBuilder().addComponents(buttons);
+
+        await interaction.editReply({ content: '<a:Alert:1271337166622167080> Are you sure you want to restart the bot?', components: [row] });
 
         const filter = (i) => i.customId === 'restart_yes' || i.customId === 'restart_no';
         const collector = interaction.channel.createMessageComponentCollector({ filter, time: 15000 });
 
         collector.on('collect', async (i) => {
+            if (i.user.id !== interaction.user.id) {
+                await i.deferReply({ ephemeral: true });
+                await i.editReply({ content: '<a:Warning:1264409367256502453> You are not authorized to use this button.', ephemeral: true });
+                return;
+            }
+
             if (i.customId === 'restart_no') {
                 await interaction.editReply({ content: 'Bot restart has been canceled.', components: [] });
                 return;
             }
 
             if (i.customId === 'restart_yes') {
-                await interaction.editReply({ content: 'Restarting the bot... Please hold on for a moment.', components: [] });
-
-                client.destroy();
+                await interaction.editReply({ content: 'Restarting the bot...', components: [] });
 
                 exec('node start.js', (error, stdout, stderr) => {
                     if (error) {
                         console.error(`exec error: ${error}`);
-                        interaction.editReply({ content: 'An error occurred during the bot restart process. Please check the console for more details.', components: [] });
+                        interaction.editReply({ content: '<a:Alert:1271337166622167080> An error occurred during the bot restart process. Please check the console for more details.', components: [] });
                         return;
                     }
                     console.log(`stdout: ${stdout}`);
                     console.error(`stderr: ${stderr}`);
 
-                    interaction.editReply({ content: 'The bot has been successfully restarted and is now back online.', components: [] });
+                    setTimeout(async () => {
+                        await interaction.deleteReply();
+                        client.destroy();
+                        process.exit();
+                    }, 1000);
                 });
             }
         });
 
         collector.on('end', (collected) => {
             if (collected.size === 0) {
-                const timeoutRow = new ActionRowBuilder()
-                    .addComponents(
-                        new ButtonBuilder()
-                            .setCustomId('end_restart_yes')
-                            .setLabel('Yes')
-                            .setStyle(ButtonStyle.Success)
-                            .setDisabled(true),
-                        new ButtonBuilder()
-                            .setCustomId('end_restart_no')
-                            .setLabel('No')
-                            .setStyle(ButtonStyle.Danger)
-                            .setDisabled(true),
-                    );
-                interaction.editReply({ content: 'No response received. Restart action timed out.', components: [timeoutRow] });
+                buttons.forEach(button => { button.setDisabled(true) });
+                const timeoutRow = new ActionRowBuilder().addComponents(buttons);
+                interaction.editReply({ content: '<a:Alert:1271337166622167080> No response received. Restart action timed out.', components: [timeoutRow] });
             }
         });
     },
